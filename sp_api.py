735d8bf7498e70ace934563242c4bf63d88e2831
nogasgofast <nogasgofast@gmail.com>
2020-03-04 01:20:45 -0600

    update to try to get database env's working to connect to mysql

--- sp_api.py
+++ sp_api.py
@@ -385,14 +385,14 @@ def db_setup(config=False):
     if sect:
         dbtype = sect.get('dbtype', os.getenv('DB_TYPE'))
         if dbtype:
-            dbconfig = (
-                sect.get('dbhost', os.getenv('DB_HOST')),
-                sect.get('dbuser', os.getenv('DB_USER')),
-                sect.get('dbpasswd', os.getenv('DB_PASSWD')),
-                sect.get('dbname', os.getenv('DB_NAME')),
-                sect.get('dbport', os.getenv('DB_PORT'))
-            )
-            sp_database.spantry.bind('mysql', *dbconfig)
+            dbconfig = {
+                'host': sect.get('dbhost', os.getenv('DB_HOST')),
+                'user': sect.get('dbuser', os.getenv('DB_USER')),
+                'passwd': sect.get('dbpasswd', os.getenv('DB_PASSWD')),
+                'db': sect.get('dbname', os.getenv('DB_NAME')),
+                'port': sect.get('dbport', os.getenv('DB_PORT'))
+            }
+            sp_database.spantry.bind(provider='mysql', **dbconfig)
         else:
             sp_database.spantry.bind('sqlite',filename='sp.sqlite', create_db=True)
     else:
