a2e1426382092b036f73f8ecfc3d72c25da7c96b
geneguido <gguido@liquidweb.com>
2022-08-05 14:46:23 -0500

    testing junk

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ app.debug = True
 #                  default_limits=[])#"200 per day",
                                   #"50 per hour"])
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True, port=6000)
