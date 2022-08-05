e005fef3d0dd70f4dc6482884f7ec9fd62eae4ef
geneguido <gguido@liquidweb.com>
2022-08-05 14:14:38 -0500

    let the debugging begin!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = appSetup(app, config)
 mail.init_app(app)
 jwt = JWTManager(app)
 images = Images(app)
+app.debug = True
 #limiter = Limiter(app,
 #                  key_func=get_remote_address,
 #                  default_limits=[])#"200 per day",
